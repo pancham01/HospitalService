36e8b2da741e94d1b26e84785de200dbeff5889b
pancham01 <pancham9557@gmail.com>
2025-11-02 18:53:32 +0530

    update

--- SpringBoot-Hospital-Service/src/main/java/com/hospital/springboot/RestContoller/HospitalController.java
+++ SpringBoot-Hospital-Service/src/main/java/com/hospital/springboot/RestContoller/HospitalController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.hospital.springboot.dto.HospitalResponse;
 import com.hospital.springboot.dto.Patient;
@@ -25,9 +24,6 @@ public class HospitalController {
 	private HospitalRepository hospitalRepository;
 
 	@Autowired
-	private RestTemplate restTemplate;
-
-	@Autowired
 	public PatientService patientService;
 	
 	@PostMapping
@@ -41,19 +37,15 @@ public class HospitalController {
 	}
 
 	// This endpoint calls Patient Service using RestTemplate
-	@GetMapping("/{hospitalId}/patient/{patientId}")
+	@GetMapping("/{hospitalId}/patients/{patientId}")
 	public HospitalResponse getHospitalWithPatient(@PathVariable(name = "hospitalId") Long hospitalId,
 			@PathVariable(name = "patientId") int patientId) {
 		Hospital hospital = hospitalRepository.findById(hospitalId).orElse(null);
 		if (hospital == null)
 			return new HospitalResponse(null, null);
 
-//        String url = "http://PATIENT-SERVICE/patients/" + patientId;
-//        Patient patient = restTemplate.getForObject(url, Patient.class);
-
+		
 		Patient patient = patientService.getPatient(patientId);
-		
-		
 		return new HospitalResponse(hospital, patient);
 	}
 }
